e0f6e4a39123abaa7aa142286c5ecb82388d2016
Luciano <lucianoaureio86@gmail.com>
2019-12-20 14:06:27 -0300

    desejando um bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,8 +7,10 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+            
             Console.WriteLine("Como voce esta hoje? ");
+
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
